0378d4de831d81673edec6e6b5afef4322469fac
your_username <your_email@gmail.com>
2022-06-10 08:42:46 +0200

    Initial Commit

---
+++ SwiftUIiOS16Feature/SwiftUIiOS16FeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIiOS16FeatureApp.swift
+//  SwiftUIiOS16Feature
+//
+//  Created by Dimitri POCHERON on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIiOS16FeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
